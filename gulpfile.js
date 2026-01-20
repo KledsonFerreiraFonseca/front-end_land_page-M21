01a39eb8d2accb5636efffbc9468d80d51a6207a
Kledson Ferreira Fonseca <kledferr@gmail.com>
2026-01-19 22:13:23 -0300

    iniciando hero do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');//configuração do gulp
 const sass = require('gulp-sass')(require('sass'));//configuração do sass
+const imagemin = require('gulp-imagemin');/*plugin de imagens*/
 
 function styles(){/*função p/ compilar SASS p/ CSS*/
     return gulp.src('./src/styles/*.scss') /*arquivos fontes*/
@@ -7,7 +8,16 @@ function styles(){/*função p/ compilar SASS p/ CSS*/
     .pipe(gulp.dest('./dist/css'));/*vamos enviar estes arquivos CSS já comprimidos para a pasta*/
 }
 
-exports.default = styles;
+/*função images*/
+function images(){/*função p/ compilar SASS p/ CSS*/
+    return gulp.src('./src/images/**/*') /*qualquer arquivo que esteja dentro de "images*/
+    .pipe(imagemin())/*execução do imagemin*/
+    .pipe(gulp.dest('./dist/images'));/*pasta de destino para comprimir*/
+}
+
+/*exportar funções*/
+exports.default = gulp.parallel(styles, images);/*p/ executar o sistema em paralelo*/
+
 exports.watch = function() {/*função para sempre atualizar as alterações do projeto*/
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
